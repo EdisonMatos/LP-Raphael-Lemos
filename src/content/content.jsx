f6afbea88829f26b9f13f3aabd0dd706160998e5
Edison Matos <edison.matos@live.com>
2025-07-03 17:39:40 -0300

    feat: Corrige link do instagram

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -100,7 +100,7 @@ export const infos = {
     </div>
   ),
   obs: <span className="italic">A Definir</span>,
-  instagramProfile: "A_definir", // Sem o @
+  instagramProfile: "adv.raphaellemos", // Sem o @
   facebookProfile: "A_Definir",
   linkeDinProfile: "A_Definir",
   x: "A_Definir",
